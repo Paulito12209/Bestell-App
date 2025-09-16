be62bed19c31937282e8c14f66fbef822e6b24a1
Son Paul <paul.angeles.chaquire@gmail.com>
2025-09-16 19:35:21 +0200

    add sticky css

--- scripts/db.js
+++ scripts/db.js
@@ -4,36 +4,36 @@ let menuItems = [
     id: 1,
     name: "Gebratene Ente süß-sauer",
     description: "knusprige Ente mit Ananas, Paprika und süß-saurer Sauce",
-    price: 14.80,
+    price: 14.80
   },
   {
     id: 2,
     name: "Kung Pao Hähnchen (scharf)",
     description: "mit Erdnüssen, Paprika und scharfer Szechuan-Sauce",
-    price: 11.90,
+    price: 11.90
   },
   {
     id: 3,
     name: "Gebratene Nudeln mit Gemüse",
     description: "Wok-Nudeln mit frischem Gemüse und Sojasauce",
-    price: 8.50,
+    price: 8.50
   },
   {
     id: 4,
     name: "Mapo Tofu (scharf)",
     description: "Seidentofu in würziger Szechuan-Sauce mit Hackfleisch",
-    price: 9.80,
+    price: 9.80
   },
   {
     id: 5,
     name: "Peking-Ente",
     description: "traditionell mit Pfannkuchen, Gurken und Hoisin-Sauce",
-    price: 18.90,
+    price: 18.90
   },
   {
     id: 6,
     name: "Dim Sum Platte",
     description: "gedämpfte Teigtaschen mit Schweinefleisch und Garnelen",
-    price: 13.50,
-  },
+    price: 13.50
+  }
 ];
