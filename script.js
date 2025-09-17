cdba6cf9d163dae18b746ce090690035140b5e69
Son Paul <paul.angeles.chaquire@gmail.com>
2025-09-18 00:19:10 +0200

    add rendercart

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // === VARIABLEN ===
 let menuItemsIndex = 0;
 let cartItems = [];
+let cartAmount = [];
 
 // === INIT FUNKTION ===
 function init() {
@@ -34,23 +35,6 @@ function renderCart() {
   ) {
     cartListRef.innerHTML += getCartItem(menuItemsIndex);
   }
-}
-
-// === TEMPLATE FUNKTIONEN ===
-function getMenuItem(menuItemsIndex) {
-  return `<div class="menu_container">
-              <div class="menu_box">
-                <div class="menu_info">
-                  <h3>${menuItems[menuItemsIndex].name}</h3>
-                  <p>${menuItems[menuItemsIndex].description}</p>
-                  <p class="menu_price">${menuItems[
-                    menuItemsIndex
-                  ].price.toFixed(2)} €</p>
-                </div>
-                <!-- BUTTON ZUM HINZUFÜGEN -->
-                <button onclick="renderCart()">+</button>
-              </div>
-            </div>`;
 }
 
 // - LÖSCHEN FUNKTION
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,4 +1,3 @@
-
 // === TEMPLATE FUNKTIONEN ===
 function getMenuItem(menuItemsIndex) {
   return `<div class="menu_container">
@@ -12,6 +11,27 @@ function getMenuItem(menuItemsIndex) {
                 </div>
                 <!-- BUTTON ZUM HINZUFÜGEN -->
                 <button onclick="renderCart()">+</button>
+              </div>
+            </div>`;
+}
+
+function getCartItem(menuItemsIndex) {
+  return `<div>
+              <h3>${cartItems[menuItemsIndex].name}</h3>
+              <div class="cart_options">
+                <div class="cart_buttons">
+                  <button onclick="decreaseAmount(${menuItemsIndex})"><img src="assets/icons/remove.png" alt="" /></button>
+                  <p>${cartAmount[i]}</p>
+                  <button onclick="increaseAmount(${menuItemsIndex})><img src="assets/icons/add.png" alt="" /></button>
+                </div>
+                <div class="cart_buttons">
+                  <p>${(
+                    cartItems[menuItemsIndex].price * cartAmount[i]
+                  ).toFixed(2)} €</p>
+                  <button>
+                    <img src="assets/icons/trash-can.png" alt="" />
+                  </button>
+                </div>
               </div>
             </div>`;
 }
