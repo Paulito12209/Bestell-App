2c9d7e7c2caa6a5ad65c26b29b24856da8f07ecf
Son Paul <paul.angeles.chaquire@gmail.com>
2025-09-17 10:23:11 +0200

    upd

--- script.js
+++ script.js
@@ -3,7 +3,17 @@
 // FUNKTIONEN:
 // - INIT FUNKTION
 function init(){
+
+}
+
 // - RENDER FUNKTION: GERICHTE
+function renderMenuItems() {
+  let menuRef = document.getElementById("gallery_content");
+  menuRef.innerHTML = "";
+
+  for (let i = 0; i < menu.length; i++) {
+    menuRef.innerHTML += getMenuItem(i);
+  }
 }
 
 // - RENDER FUNKTION: WARENKORB?
